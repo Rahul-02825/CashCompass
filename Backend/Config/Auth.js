77cb2ad5fc92bc17b8c61cc42ef5424151c4e9a0
Rahul-02825 <rahul2825.kannan@gmail.com>
2024-06-02 15:20:15 +0530

    authentication done

---
+++ Backend/Config/Auth.js
@@ -0,0 +1,37 @@
+const passport=require("passport")
+const LocalStrategy=require('passport-local').Strategy
+const User=require("../Models/Usermodel");
+const { compareSync } = require("bcrypt");
+
+passport.use(new LocalStrategy(
+    async function(username, password, done) {
+        try {
+            const user = await User.findOne({ username: username });
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+            if(!compareSync(password,user.password)) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    }
+));
+
+// Serialize user information into session
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+    console.log(user.id)
+});
+
+// Deserialize user information from session
+passport.deserializeUser(async function(id, done) {
+    try {
+        const user = await User.findById(id).exec();
+        done(null, user);
+    } catch (err) {
+        done(err, null);
+    }
+});
